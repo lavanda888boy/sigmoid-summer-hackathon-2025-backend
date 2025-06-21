d923d74a61b4927fd776d71f7f60c4c0a3036658
Sevastian Bajenov <sevabaj@gmail.com>
2025-06-22 01:43:34 +0300

    chore: uncomment repo parsing call.

--- repo-parsing/main.py
+++ repo-parsing/main.py
@@ -2,8 +2,8 @@ from parser.parser import search_repositories_with_open_issues, save_results_to_
 
 
 if __name__ == "__main__":
-  # results = search_repositories_with_open_issues()
-  # save_results_to_json(results)
+  results = search_repositories_with_open_issues()
+  save_results_to_json(results)
 
   repos = read_repos_from_json()
   send_repos_to_message_broker(repos)
