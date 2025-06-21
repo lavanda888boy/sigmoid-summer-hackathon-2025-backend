ca176c7df2f1be41fa29d297a8e3395d77f4bbfa
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2025-06-21 21:29:06 +0300

    Implement pref-s customization

--- app/myauth/urls.py
+++ app/myauth/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('callback/', views.github_oauth_callback, name='github_oauth_callback'),
+    path('callback/', views.github_oauth_callback, name='github-oauth-callback'),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from users.serializers import UserPreferencesUpdateSerializer
 
-# Create your views here.
+
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def update_user_preferences(request):
+    """
+    Overwrites the user's preferred programming languages and domains
+    """
+    user = request.user
+    serializer = UserPreferencesUpdateSerializer(user, data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            'message': 'Preferences updated successfully.',
+        }, status=status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
